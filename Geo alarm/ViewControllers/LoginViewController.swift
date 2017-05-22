231e27f8cb6be70f65a4689bd0c1847e5887cd2f
Andrew Zaiets <andrew.zaec@gmail.com>
2017-05-23 00:54:26 +0300

    init

---
+++ Geo alarm/ViewControllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Geo alarm
+//
+//  Created by Andrew Zaiets on 19.05.17.
+//  Copyright © 2017 Armadillo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
